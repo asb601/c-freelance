be75b72b963c7e1b00f9916309d8504e31d0e97e
asb601 <codeen601@gmail.com>
2025-01-01 21:56:38 +0530

    update-01

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+
 import App from "./App"; // User Routes
  // Admin Routes
 import "./index.css"; // Import TailwindCSS styles
-import store from "./redux/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+   
       <BrowserRouter>
         <Routes>
           {/* User Routes */}
@@ -19,6 +19,6 @@ root.render(
 
         </Routes>
       </BrowserRouter>
-    </Provider>
+    
   </React.StrictMode>
 );
